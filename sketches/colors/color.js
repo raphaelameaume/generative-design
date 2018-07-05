55048f563218c532e4ab60181bdd67c337820e68
Raphaël Améaume <raphael@akufen.ca>
2018-07-05 18:55:51 -0400

    remove unused stuff

--- sketches/colors/color.js
+++ sketches/colors/color.js
@@ -1,6 +1,4 @@
 import { PaperSize, Orientation } from 'penplot';
-import { randomFloat, setSeed } from 'penplot/util/random';
-import { clipPolylinesToBox } from 'penplot/util/geom';
 import Color from 'color';
 
 export const orientation = Orientation.LANDSCAPE;
@@ -24,21 +22,6 @@ export default function createPlot (context, dimensions) {
         mouse.y = y;
     });
 
-    const lines = new Array(lineCount).fill(undefined).map((_, j) => {
-        const angleOffset = randomFloat(-Math.PI * 2, Math.PI * 2);
-        const angleScale = randomFloat(0.001, 1);
-
-        return new Array(segments).fill(undefined).map((_, i) => {
-            const t = i / (segments - 1);
-            const angle = (Math.PI * 2 * t + angleOffset) * angleScale;
-            const x = Math.cos(angle);
-            const y = Math.sin(angle);
-            const offset = j * 0.2;
-            const r = radius + offset;
-            return [ x * r + width / 2, y * r + height / 2 ];
-        });
-    });
-
     const innerColor = Color().hsl();
     innerColor.color[1] = 50;
     innerColor.color[2] = 50;
@@ -49,7 +32,6 @@ export default function createPlot (context, dimensions) {
   
     return {
         draw,
-        print,
         animate: true,
         background: 'white' // used when exporting the canvas to PNG
     };
@@ -70,19 +52,5 @@ export default function createPlot (context, dimensions) {
         context.fillStyle = c1;
 
         context.fillRect(width * 0.5 - size * 0.5, height * 0.5 - size * 0.5, size, size);
-
-        // context.lineWidth = 0.1;
-
-        // lines.forEach(points => {
-        //     context.beginPath();
-        //     points.forEach(p => context.lineTo(p[0], p[1]));
-        //     context.stroke();
-        // });
-    }
-  
-    function print () {
-        return polylinesToSVG(lines, {
-            dimensions
-        });
     }
   }
