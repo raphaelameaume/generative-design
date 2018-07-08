4a5777f7c26882834df62b170fbe3a852d5b31ca
Raphaël Améaume <raphael.ameaume@gmail.com>
2018-07-08 11:56:06 -0400

    pixel sorting

--- sketches/colors/image.js
+++ sketches/colors/image.js
@@ -28,6 +28,49 @@ const sortByBrightness = ( colors ) => {
     return hslColors;
 };
 
+const sortBySaturation = ( colors ) => {
+    const hslColors = colors.map( ( color ) => {
+        return color.hsl();
+    });
+
+    hslColors.sort( ( a, b ) => {
+        return a.color[1] < b.color[1]
+    });
+
+    return hslColors;
+};
+
+const sortByChannel = ( colors, channel ) => {
+    colors.sort( ( a, b ) => {
+        return a.color[channel] < b.color[channel]
+    });
+
+    return colors;
+};
+
+const sortImageData =  ( data, w, h ) => {
+    const sortedData = [];
+
+    for ( let i = 0; i < data.length; i+= 4 ) {
+        const r = data[i];
+        const g = data[i + 1];
+        const b = data[i + 2];
+        const a = data[i + 3];
+
+        const m = i / 4;
+        const y = Math.floor(m / h);
+        const x = m - y * w;
+
+        if ( !sortedData[x] ) {
+            sortedData[x] = [];
+        }
+
+        sortedData[x][y] = [ r, g, b, a ];
+    }
+
+    return sortedData;
+};
+
 export default function createPlot (context, dimensions) {
     const [ width, height ] = dimensions;
 
@@ -42,9 +85,9 @@ export default function createPlot (context, dimensions) {
         draw();
     }
 
-    image.src = '../../assets/buildings.jpeg';
+    image.src = '../../assets/beach.jpg';
 
-
+    let time = 0;
 
     window.addEventListener('keypress', () => {
     })
@@ -52,14 +95,14 @@ export default function createPlot (context, dimensions) {
     return {
         draw,
         animate: false,
-        background: 'white' // used when exporting the canvas to PNG
+        background: 'black' // used when exporting the canvas to PNG
     };
   
     function draw () {
+        time++;
+
         if ( loaded && drawn && !imageData ) {
             imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-
-            console.log('get image data', imageData);
             draw();
         }
         
@@ -67,49 +110,48 @@ export default function createPlot (context, dimensions) {
             context.drawImage(image, 0, 0, canvas.width, canvas.height);
             drawn = true;
             draw();
-
-            // console.log(imageData);
         }
 
         if ( loaded && imageData ) {
             loaded = false;
-            const tileCount = canvas.width / 25;
+            const tileCount = 200;
+
             const rectSize = canvas.width / tileCount;
             const colors = [];
             const { data } = imageData;
 
+            const sortedData = sortImageData(data, canvas.width, canvas.height);
+
             for ( let gridY = 0; gridY < tileCount; gridY++ ) {
                 for ( let gridX = 0; gridX < tileCount; gridX++ ) {
-                    const px = gridX * rectSize;
-                    const py = gridY * rectSize;
+                    const px = Math.floor(gridX * rectSize);
+                    const py = Math.floor(gridY * rectSize);
 
-                    // console.log(px, py);
+                    const [ r, g, b, a ] = sortedData[px][py];
 
-                    for ( let i = 0; i < data.length; i+= 4 ) {
-                        const r = data[i];
-                        const g = data[i + 1];
-                        const b = data[i + 2];
-                        const a = data[i + 3];
-
-                        const m = i / 4;
-                        const y = Math.floor(m / canvas.height);
-                        const x = m - y * canvas.width;
-
-                        if ( px === x && y === py ) {
-                            colors.push(Color({r, g, b}).alpha(a / 255));
-                        }
-                    }
+                    colors.push(Color({r, g, b}).alpha(a / 255));
                 }
             }
 
+            // const sortedColors = sortByHue(colors);
+            // const sortedColors = sortByChannel(colors, 0);
             const sortedColors = sortByBrightness(colors);
+            // const sortedColors = sortBySaturation(colors);
 
             let i = 0;
 
+            context.clearRect(0, 0, canvas.width, canvas.height);
+
             for ( let gridY = 0; gridY < tileCount; gridY++ ) {
                 for ( let gridX = 0; gridX < tileCount; gridX++ ) {
-                    context.fillStyle = colors[i].hex();
-                    context.fillRect(gridX * rectSize, gridY * rectSize, rectSize, rectSize);
+                    context.fillStyle = sortedColors[i].hex();
+
+                    const radius = rectSize * 0.5;
+
+                    context.beginPath();
+                    context.arc(gridX * rectSize + radius, gridY * rectSize + radius, radius, 0, 2 * Math.PI, false);
+                    context.fill();
+                    // context.fillRect(gridX * rectSize, gridY * rectSize, rectSize, rectSize);
                     i++;
                 }
             }
